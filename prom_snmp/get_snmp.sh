808ffa3a7900cb78564e727996667dae9b8177b9
sid <chrngtingliu@systex.com>
2022-01-25 16:40:22 +0800

    test

--- prom_snmp/get_snmp.sh
+++ prom_snmp/get_snmp.sh
@@ -1,1 +1,6 @@
 docker run --rm -it -v "${PWD}:/opt" -v "mibs:/root/.snmp/mibs" prom/snmp-generator generate
+sed  -i 's/hh3cEntityExtCpuUsage/SwitchCpuUsage/g' snmp.yml
+sed  -i 's/hh3cEntityExtMemUsage/SwitchMemUsage/g' snmp.yml
+sed  -i 's/ciscoMemoryPoolUsed/SwitchMemUsed/g' snmp.yml
+sed  -i 's/ciscoMemoryPoolFree/SwitchMemFree/g' snmp.yml
+sed  -i 's/cpmCPUTotal5minRev/SwitchCpuUsage/g' snmp.yml
---
+++ prom_snmp/rename_snmp.sh
@@ -0,0 +1,5 @@
+sed  -i 's/hh3cEntityExtCpuUsage/SwitchCpuUsage/g' snmp.yml
+sed  -i 's/hh3cEntityExtMemUsage/SwitchMemUsage/g' snmp.yml
+sed  -i 's/ciscoMemoryPoolUsed/SwitchMemUsed/g' snmp.yml
+sed  -i 's/ciscoMemoryPoolFree/SwitchMemFree/g' snmp.yml
+sed  -i 's/cpmCPUTotal5minRev/SwitchMemFree/g' snmp.yml
